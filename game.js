3b223ca4e26e42f00ea46ce620f958c6fc6c6655
Sudhakar <asudhakar@live.in>
2015-09-04 11:19:03 +0530

    just placed the basement for 2048

---
+++ game.js
@@ -0,0 +1,35 @@
+var gameObj = {
+	points: {
+		score: 0,
+		history: [];
+		status: 1
+	},
+
+	stage: [],
+
+	intiStage: function () {
+		for (var cell = 0; cell < 4; cell++) {
+			this.stage[cell]=[];
+			for(var row = 0; row < 4; row++ ){
+				this.stage[cell][row] = {
+					boxObj: null,
+					position: [cell, row]
+				};
+			}
+		}
+	},
+
+	empty:function  () {
+		var emptyList =[];
+		for(var row = 0; row < 4; row ++){
+			for(var cell = 0; cell <4; cell++){
+				if(this.stage[cell][row].boxObj == null) {
+					emptyList.push(this.stage[cell][row]);
+				}
+			}
+		}
+		return emptyList;
+	},
+
+	
+}
