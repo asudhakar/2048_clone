a5462df85a7d3c5cd9802dc7e5a4d045e19aa8d0
Sudhakar <asudhakar@live.in>
2015-09-04 15:07:20 +0530

    modified in game.js about disablesection

--- game.js
+++ game.js
@@ -227,3 +227,14 @@ var controller = function () {
 		end: this.end
 	}
 }();
+
+function disableSelection(target){
+	if (typeof target.onselectstart!="undefined")
+		target.onselectstart=function(){return false}
+	else if (typeof target.style.MozUserSelect!="undefined")
+		target.style.MozUserSelect="none"
+	else
+		target.onmousedown=function(){return false}
+	target.style.cursor = "default";
+}
+
